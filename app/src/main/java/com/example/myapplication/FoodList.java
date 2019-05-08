49937aec5d94fb90283b18a32a0453d42237ab96
nikheal25 <nikheal25@gmail.com>
2019-05-09 05:00:04 +1000

    Added NDB call and search new food item

--- app/src/main/java/com/example/myapplication/FoodList.java
+++ app/src/main/java/com/example/myapplication/FoodList.java
@@ -1,6 +1,6 @@
 package com.example.myapplication;
 
-import android.content.Context;
+import android.app.Fragment;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.net.Uri;
@@ -8,31 +8,33 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.app.Fragment;
-import android.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
+
+;
 
 
 /**
@@ -43,12 +45,15 @@ import java.util.Scanner;
  * Use the {@link FoodList#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class FoodList extends Fragment {
+public class FoodList extends Fragment implements View.OnClickListener{
     private View view;
     private OnFragmentInteractionListener mListener;
     ArrayList<Food> foods;
     ArrayList<String> foodItems;
     Spinner subSpinner, spinner;
+    private EditText searchFoodItemName;
+    private TextView foodId, foodName, foodCalories, foodUnit, foodAmount, foodfat;
+    private Button searchButton;
 
 
 
@@ -67,7 +72,14 @@ public class FoodList extends Fragment {
         view.setBackgroundColor(Color.WHITE);
 
         spinner = (Spinner) view.findViewById(R.id.foodCategory1);
-        subSpinner = (Spinner) view.findViewById(R.id.spinner2); //TODO not working
+        subSpinner = (Spinner) view.findViewById(R.id.spinner2);
+
+        foodId = (TextView) view.findViewById(R.id.textViewFoodItemNo2);
+        foodName = (TextView) view.findViewById(R.id.textViewFoodItemName);
+        foodCalories = (TextView) view.findViewById(R.id.textViewFooditemCalories);
+        foodUnit = (TextView) view.findViewById(R.id.textViewFoodItemServing);
+        foodAmount = (TextView) view.findViewById(R.id.textViewFoodItemAmount);
+        foodfat = (TextView) view.findViewById(R.id.textViewFoodItemFat);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.food_category, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -100,7 +112,37 @@ public class FoodList extends Fragment {
 
             }
         });
+
+        //Search operation
+        searchFoodItemName = (EditText) view.findViewById(R.id.newFoodItemName);
+        searchButton = (Button) view.findViewById(R.id.buttonNewFoodItemSearch);
+
+        searchButton.setOnClickListener(this);
+
         return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        try{
+            NDBAccess ndbAccess =new NDBAccess();
+            String foodItemName = searchFoodItemName.getText().toString();
+            if(foodItemName.length()>0){
+                ArrayList<String> resultSet = ndbAccess.execute(foodItemName).get();
+                if (resultSet.size() != 6){
+                    throw new Exception();
+                }
+                foodName.setText(resultSet.get(0));
+                foodId.setText(resultSet.get(1));
+                foodCalories.setText(resultSet.get(2));
+                foodUnit.setText(resultSet.get(3));
+                foodAmount.setText(resultSet.get(4));
+                foodfat.setText(resultSet.get(5));
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     private String[] returnSpecificCategories(String category){
@@ -131,6 +173,8 @@ public class FoodList extends Fragment {
         getActivity().setTitle("Food Items");
     }
 
+
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -145,6 +189,98 @@ public class FoodList extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+}
+
+class NDBAccess extends AsyncTask<String,Void, ArrayList<String>> {
+    private static final String BASE_URL = "https://api.nal.usda.gov/ndb/search/?format=json&q=";
+    private static final String END_URL = "&sort=n&max=1&offset=0&api_key=12qSIATkTYDzE5u3KLxL5iUqAEHN8MZYJuyQHn5N";
+
+    private static final String QUERY_BASE_URL = "https://api.nal.usda.gov/ndb/reports/?ndbno=";
+    private static final String QUERY_END_URL = "&type=f&format=json&api_key=12qSIATkTYDzE5u3KLxL5iUqAEHN8MZYJuyQHn5N";
+
+    private HttpURLConnection createConnectio(String startString, String middleString, String lastString){
+        HttpURLConnection connection =null;
+        URL ndbURL =null;
+        try{
+            ndbURL = new URL(startString+middleString+lastString);
+            connection =  (HttpURLConnection) ndbURL.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    @Override
+    protected ArrayList<String> doInBackground(String... param) {
+        String foodName = param[0];
+
+        URL ndbURL;
+        ArrayList<String> resultSet = new ArrayList<String>();
+        resultSet.add(foodName);
+        String textResult = "";
+        HttpURLConnection connection = null;
+
+        try {
+            // credential = new URL("http://10.0.2.2:8080/assgn/webresources/restws.appuser/findByName/nik");
+            ndbURL = new URL(BASE_URL+foodName+END_URL);
+            connection =  (HttpURLConnection) ndbURL.openConnection();
+            connection.setRequestMethod("GET");
+
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode!=200){
+                resultSet.add("ERROR");
+            }else {
+                InputStream inputStream = connection.getInputStream();
+                Scanner scanner = new Scanner(inputStream);
+                while (scanner.hasNextLine()) {
+                    textResult += scanner.nextLine();
+                }
+                JsonParser parser = new JsonParser();
+
+
+                String ndbFoodId = parser.parse(textResult).getAsJsonObject().get("list").getAsJsonObject().get("item").getAsJsonArray().get(0).getAsJsonObject().get("ndbno").getAsString();
+                resultSet.add(ndbFoodId);
+                connection = createConnectio(QUERY_BASE_URL, ndbFoodId, QUERY_END_URL);
+
+                textResult = "";
+                inputStream = connection.getInputStream();
+                scanner = new Scanner(inputStream);
+                while (scanner.hasNextLine()) {
+                    textResult += scanner.nextLine();
+                }
+
+                parser = new JsonParser();
+                JsonObject jsonObject = (JsonObject) parser.parse(textResult).getAsJsonObject().get("report").getAsJsonObject().get("food").getAsJsonObject().get("nutrients").getAsJsonArray().get(0);
+
+                resultSet.add(jsonObject.get("value").getAsString());
+                resultSet.add(jsonObject.get("measures").getAsJsonArray().get(0).getAsJsonObject().get("label").getAsString());
+                resultSet.add(jsonObject.get("measures").getAsJsonArray().get(0).getAsJsonObject().get("qty").getAsString());
+                resultSet.add(jsonObject.get("measures").getAsJsonArray().get(0).getAsJsonObject().get("value").getAsString());
+               // JsonReader jsonReader = new JsonReader(textResult);
+
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(connection != null)
+                connection.disconnect();
+        }
+        return resultSet;
+    }
+
 }
 
 class FoodTableQuery extends AsyncTask<Void,Void, ArrayList<Food>> {
